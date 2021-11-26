6408aff04ae1aba1e87558ef83b3789da34778fe
Elshaddai <elshaddai.excel@ti.ukdw.ac.id>
2021-11-26 09:49:50 +0700

    71160108_Tambah Soal 3

---
+++ 3_D_71160101.py
@@ -0,0 +1,18 @@
+def GenerateSoal():
+    perbandingan = ('<','>','==')
+    import random
+    print("") 
+
+def cekHasil():
+    if masuk == "penjumlahan":
+        print("benar/salah jika ")
+        GenerateSoal()
+    elif masuk == "pengurangan":
+        print("benar/salah jika ")
+        GenerateSoal()
+    else: print("end")
+
+
+masuk = str(input("masukan tipe yang ingin anda coba"))
+
+cekHasil()
